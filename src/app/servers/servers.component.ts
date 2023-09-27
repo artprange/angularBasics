b7a613b627869e3fbef6640014fdc58cc1dc163c
artprange <arthur.prange@gmail.com>
2023-09-27 19:45:03 -0300

    (features): databinding

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -9,22 +9,39 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ServersComponent implements OnInit {
   allowNewServer = false;
+  allowNewUser = false;
+  userCreationStatus = 'no user was added';
   serverCreationStatus = 'No server was created!';
-  serverName = 'TestServer';
+  serverName = '';
+  userName = '';
 
   constructor() {
     setTimeout(() => {
-      this.allowNewServer = true;
+      this.allowNewUser = true;
     }, 2000);
+    setTimeout(() => {
+      this.allowNewServer = true;
+    }, 5000);
   }
+
   ngOnInit() {}
 
+  onCreateUser() {
+    this.userCreationStatus = 'User was created!' + this.userName;
+  }
+  onUpdateUser(event: any) {
+    this.userName = (<HTMLInputElement>event.target).value;
+    this.userName = '';
+  }
+
   onCreateServer() {
-    this.serverCreationStatus = 'Server was created!';
+    this.serverCreationStatus = 'Server was created!' + this.serverName;
   }
 
   onUpdateServerName(event: any) {
-    this.serverName = (<HTMLInputElement>event.target).value; //it would be like this if TS doesn't like it.
+    this.serverName = (<HTMLInputElement>event.target).value;
+    this.serverName = '';
+    //it would be like this if TS doesn't like it.
     // update: TS didn't like it. it didn't work as intended and in had to specify that it was indeed a HTML element. brat.
   }
 }
