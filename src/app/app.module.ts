77d37b0774e4e48c8fcb04a9b3497a7300d57491
artprange <arthur.prange@gmail.com>
2023-09-27 17:43:16 -0300

    features:(possible language mode issues. investigating.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,21 +1,22 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { ServerComponent } from './server/server.component';
 import { ServersComponent } from './servers/servers.component';
 import { SucessAlertComponent } from './sucess-alert/sucess-alert.component';
-import { WaringAlertComponent } from './warning-alert/warning-alert.cmponent';
+import { WarningAlertComponent } from './warning-alert/warning-alert.cmponent';
 
 @NgModule({
   declarations: [
     AppComponent,
     ServerComponent,
     ServersComponent,
-    WaringAlertComponent,
+    WarningAlertComponent,
     SucessAlertComponent,
   ],
-  imports: [BrowserModule],
+  imports: [BrowserModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent],
 })
--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -10,6 +10,7 @@ import { Component, OnInit } from '@angular/core';
 export class ServersComponent implements OnInit {
   allowNewServer = false;
   serverCreationStatus = 'No server was created!';
+  serverName = 'TestServer';
 
   constructor() {
     setTimeout(() => {
@@ -20,5 +21,10 @@ export class ServersComponent implements OnInit {
 
   onCreateServer() {
     this.serverCreationStatus = 'Server was created!';
+  }
+
+  onUpdateServerName(event: any) {
+    this.serverName = (<HTMLInputElement>event.target).value; //it would be like this if TS doesn't like it.
+    // update: TS didn't like it. it didn't work as intended and in had to specify that it was indeed a HTML element. brat.
   }
 }
