b562e6938454ae11355f4e583a1792d2d8c63a65
artprange <arthur.prange@gmail.com>
2023-09-29 11:39:24 -0300

    (features): conditional styling

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -48,4 +48,6 @@ export class ServersComponent implements OnInit {
     //it would be like this if TS doesn't like it.
     // update: TS didn't like it. it didn't work as intended and in had to specify that it was indeed a HTML element. brat.
   }
+
+  
 }
---
+++ src/app/task2/task2.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-task2',
+  templateUrl: './task2.component.html',
+  styleUrls: ['./task2.component.css'],
+})
+export class Task2Component {
+  showSecret = false;
+  log: number[] = [];
+
+  onToggleDetails() {
+    this.showSecret = !this.showSecret;
+    this.log.push(this.log.length + 1);
+  }
+}
